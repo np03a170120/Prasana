ad10584af50f930f9193f7f16db39cde1bd39a15
dreamCode <np03a170120@heraldcollege.edu.np>
2022-01-07 17:28:24 +0545

    IC

---
+++ header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/slick.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./fontawesome-free-5.15.2-web/css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./css/main.css">
+
+    <title>Prasana</title>
+</head>
+
+<body>
+    <header class="header-main">
+        <div class="container">
+            <div class="header-wrapper">
+                <div class="header-logo">
+                    <img src="./images/logo.png" alt="">
+                </div>
+                <div class="header-mid">
+                    <nav class="navbar navbar-expand-lg">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Team</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Dropdown link
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <a class="dropdown-item" href="#">Gallery</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <div class="header-right">
+                    <div class="hr-img">
+                        <img src="./images/header-image.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'header.php'?>
+<section class="main-about sec-padding">
+    <div class="container">
+        <div class="sec-title-about">
+            <h6>welcome to</h6>
+            <h5>Prasana International<span>
+                    Consultancy
+                </span> </h5>
+        </div>
+        <div class="ma-text">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse animi aliquid, amet repudiandae nemo unde
+                quas quo quasi doloremque maiores soluta praesentium atque ipsum pariatur numquam laboriosam.
+                Architecto, sunt aspernatur.</p>
+        </div>
+        <div class="learn-more">
+            <a href="#">Get started</a>
+        </div>
+    </div>
+</section>
+
+<?php include 'footer.php'?>
