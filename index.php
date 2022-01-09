c218b0c087d4fadf10ccc3f813fbe62981e5ce57
dreamCode <np03a170120@heraldcollege.edu.np>
2022-01-09 13:19:32 +0545

    next stat section

--- index.php
+++ index.php
@@ -18,4 +18,162 @@
     </div>
 </section>
 
+<section class="main-about-image">
+    <div class="container no-pad">
+        <div class="main-about-image-slider">
+            <div class="mais-single">
+                <img src="./images/about-banner-img.png" alt="">
+            </div>
+            <div class="mais-single">
+                <img src="./images/about-banner-img-1.png" alt="">
+            </div>
+            <div class="mais-single">
+                <img src="./images/about-banner-img.png" alt="">
+            </div>
+            <div class="mais-single">
+                <img src="./images/about-banner-img-2.png" alt="">
+            </div>
+            <div class="mais-single">
+                <img src="./images/about-banner-img.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="main-services sec-padding">
+    <div class="container">
+        <div class="sec-title">
+            <h2>our services</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="ms-single">
+                    <div class="mss-left">
+                        <i class="fas fa-folder-open"></i>
+                    </div>
+                    <div class="mss-right">
+                        <div class="mssr-title">
+                            <h6>Document & Financial Guidence</h6>
+                        </div>
+                        <div class="mssr-content">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam non corporis
+                                mollitia natus praesentium exercitationem. Earum vitae dolore natus magnam </p>
+                        </div>
+                        <div class="learn-more-plain">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="ms-single">
+                    <div class="mss-left">
+                        <i class="fas fa-user-friends"></i>
+                    </div>
+                    <div class="mss-right">
+                        <div class="mssr-title">
+                            <h6>Counselling</h6>
+                        </div>
+                        <div class="mssr-content">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam non corporis
+                                mollitia natus praesentium exercitationem. Earum vitae dolore natus magnam </p>
+                        </div>
+                        <div class="learn-more-plain">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="ms-single">
+                    <div class="mss-left">
+                        <i class="fab fa-cc-visa"></i>
+                    </div>
+                    <div class="mss-right">
+                        <div class="mssr-title">
+                            <h6>Visa application process</h6>
+                        </div>
+                        <div class="mssr-content">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam non corporis
+                                mollitia natus praesentium exercitationem. Earum vitae dolore natus magnam </p>
+                        </div>
+                        <div class="learn-more-plain">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="ms-single">
+                    <div class="mss-left">
+                        <i class="fas fa-chalkboard-teacher"></i>
+                    </div>
+                    <div class="mss-right">
+                        <div class="mssr-title">
+                            <h6>Visa application process</h6>
+                        </div>
+                        <div class="mssr-content">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam non corporis
+                                mollitia natus praesentium exercitationem. Earum vitae dolore natus magnam </p>
+                        </div>
+                        <div class="learn-more-plain">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="main-booking sec-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 disp-flex j-c-c">
+                <div class="mb-left-wrapper ">
+                    <div class="mb-title">
+                        <h4>Do you want us to help you out?</h4>
+                    </div>
+                    <div class="mb-text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, porro. Recusandae quisquam,
+                            cum
+                            vitae architecto explicabo sint neque sequi deleniti odio qui? Non error vel quae rem enim,
+                            assumenda pariatur?</p>
+                    </div>
+                    <div class="book-ielts">
+                        <a href="#">Book ilets class</a>
+                    </div>
+                    <div class="book-ielts apply-online">
+                        <a href="#">apply online</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 disp-flex j-c-c">
+                <div class="mb-image">
+                    <img src="./images/main-booking.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+<section class="main-stat sec-padding">
+    <div class="container">
+        <div class="sec-title">
+            <h2>our stats</h2>
+            <h6>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum quae, ea porro totam dolores aliquam
+                eligendi, numquam ipsa quidem magni vero perferendis corporis, in ex perspiciatis nobis quas facere
+                similique.</h6>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div class="ms-single">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include 'footer.php'?>
